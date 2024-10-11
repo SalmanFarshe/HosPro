f28c60adb11c4124f8aeafd01dfee19f8fc65989
Salman Farshe <salman.zihad@involve.ai>
2024-10-12 01:15:03 +0600

    modified all page for active menu setting

--- src/components/sidenav.php
+++ src/components/sidenav.php
@@ -8,34 +8,34 @@
 
     <!-- Vertical Tab Links -->
     <nav class="dashboard-nav-list">
-        <a href="../pages/dashboard.php" class="dashboard-nav-item <?php if($activePage == 'dashboard') echo 'active'; ?>">
+        <a href="../pages/dashboard.php" class="dashboard-nav-item <?php if($activePage == 'dashboard') echo 'active-page'; ?>">
             <i class="fas fa-tachometer-alt"></i> Dashboard
         </a>
-        <a href="../pages/room-mapping.php" class="dashboard-nav-item <?php if($activePage == 'room-mapping') echo 'active'; ?>">
+        <a href="../pages/room-mapping.php" class="dashboard-nav-item <?php if($activePage == 'room-mapping') echo 'active-page'; ?>">
             <i class="fa-solid fa-bed"></i> Room Mapping
         </a>
-        <a href="../pages/employee.php" class="dashboard-nav-item <?php if($activePage == 'employee') echo 'active'; ?>">
+        <a href="../pages/employee.php" class="dashboard-nav-item <?php if($activePage == 'employee') echo 'active-page'; ?>">
             <i class="fa-solid fa-user-tie"></i> Employee
         </a>
-        <a href="../pages/events.php" class="dashboard-nav-item <?php if($activePage == 'events') echo 'active'; ?>">
+        <a href="../pages/events.php" class="dashboard-nav-item <?php if($activePage == 'events') echo 'active-page'; ?>">
             <i class="fa-solid fa-calendar-days"></i> All Events
         </a>
-        <a href="../pages/allusers.php" class="dashboard-nav-item <?php if($activePage == 'allusers') echo 'active'; ?>">
+        <a href="../pages/allusers.php" class="dashboard-nav-item <?php if($activePage == 'allusers') echo 'active-page'; ?>">
             <i class="fa-solid fa-users"></i> All Users
         </a>
 
         <!-- Packages Section -->
-        <a href="../pages/packages.php" class="dashboard-nav-item <?php if($activePage == 'packages') echo 'active'; ?>">
+        <a href="../pages/packages.php" class="dashboard-nav-item <?php if($activePage == 'packages') echo 'active-page'; ?>">
             <i class="fa-solid fa-clipboard-list"></i> Packages
         </a>
 
-        <a href="../pages/billandpayments.php" class="dashboard-nav-item <?php if($activePage == 'billandpayments') echo 'active'; ?>">
+        <a href="../pages/billandpayments.php" class="dashboard-nav-item <?php if($activePage == 'billandpayments') echo 'active-page'; ?>">
             <i class="fa-regular fa-credit-card"></i> Bill & Payments
         </a>
-        <a href="../pages/costing.php" class="dashboard-nav-item <?php if($activePage == 'costing') echo 'active'; ?>">
+        <a href="../pages/costing.php" class="dashboard-nav-item <?php if($activePage == 'costing') echo 'active-page'; ?>">
             <i class="fa-solid fa-chart-pie"></i> Total Costing
         </a>
-        <a href="../pages/customize.php" class="dashboard-nav-item <?php if($activePage == 'customize') echo 'active'; ?>">
+        <a href="../pages/customize.php" class="dashboard-nav-item <?php if($activePage == 'customize') echo 'active-page'; ?>">
             <i class="fa-solid fa-pen-to-square"></i> Customization
         </a>
     </nav>
